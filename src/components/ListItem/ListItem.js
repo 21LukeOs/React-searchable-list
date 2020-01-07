6126a5cceb83be8b07403a02a1de9ec47b2103f0
Luke Osgood <lukemosgood@gmail.com>
2020-01-07 01:37:11 -0500

    getting close to finishing add tag functionality

--- src/components/ListItem/ListItem.js
+++ src/components/ListItem/ListItem.js
@@ -4,53 +4,105 @@ import plus from '../../assets/plus-icon.svg';
 import minus from '../../assets/minus-icon.svg';
 
 class ListItem extends React.Component {
+	state = {
+		isToggleGradesOn: false
+	};
 
-  state = {
-    isToggleGradesOn: false
-  }
-
-  handleClickGrades = () => {
-    this.setState(state => ({
-      isToggleGradesOn: !state.isToggleGradesOn
-    }));
-  }
-
-  render() {
-    const { grades, pic, firstName, lastName, email, company, skill } = this.props.student;
-
-    const gradesTypeNumber = grades.map(grade => {
-      return parseInt(grade, 10);
-    });
-
-    const average = gradesTypeNumber.reduce((a, b) => a + b, 0) / grades.length;
-    
-    const fullName = `${firstName} ${lastName}`;
-
-    const allGrades = grades.map((grade, i) => {
-      return <li className='each-grade' key={grade * Math.random()}><span>Test {i + 1}:</span><span>{grade}%</span></li>;
-    })
-
-    return (
-      <div className='list-item'>
-        <img className='list-item__img' src={pic} alt={fullName} />
-        <div className='list-item__info'>
-          <h1 className='list-item__name'>{firstName} {lastName}</h1>
-          <ul className='list-item__list'>
-            <li className='list-item__li list-item__email'>Email: {email}</li>
-            <li className='list-item__li list-item__company'>Company: {company}</li>
-            <li className='list-item__li list-item__skill'>Skill: {skill}</li>
-            <li className='list-item__li list-item__average'>Average: {average.toFixed(2)}%</li>
-          </ul>
-          <ul className='all-grades'>
-            { this.state.isToggleGradesOn ? allGrades : !allGrades }
-          </ul>
-        </div>
-        <div className='plus-minus-cont'>
-          { this.state.isToggleGradesOn ? <img className='minus' src={minus} alt='subtract' onClick={this.handleClickGrades} /> : <img className='plus' src={plus} alt='expand' onClick={this.handleClickGrades} /> }
-        </div>
-      </div>
-    );
+	handleClickGrades = () => {
+		this.setState(state => ({
+			isToggleGradesOn: !state.isToggleGradesOn
+		}));
   };
-};
+  
+  handleChange = (e) => {
+    if (e.keyCode === 13 && e.target.value !== '') {
+      console.log(e.target.value, e.target.id)
+      this.props.setTags(e.target.value, e.target.id);
+      e.target.value = '';
+    }
+	};
+
+	render() {
+		const {
+      id,
+			grades,
+			pic,
+			firstName,
+			lastName,
+			email,
+			company,
+      skill,
+      tags
+		} = this.props.student;
+
+		const gradesTypeNumber = grades.map(grade => {
+			return parseInt(grade, 10);
+		});
+
+		const average = gradesTypeNumber.reduce((a, b) => a + b, 0) / grades.length;
+
+    const fullName = `${firstName} ${lastName}`;
+    
+    // const allTags = tags.map(tag => {
+    //   return <div className='tag' key={Math.random()}>{tag}</div>
+    // })
+
+		const allGrades = grades.map((grade, i) => {
+			return (
+				<div className='each-grade' key={grade * Math.random()}>
+					<span>Test {i + 1}:</span>
+					<span>{grade}%</span>
+				</div>
+			);
+		});
+
+		const gradesAndTags = (
+			<>
+				<div className='all-grades'>{allGrades}</div>
+				<div className='tags'>{tags ? tags.map(tag => {return <div className='tag' key={Math.random()}>{tag}</div>}) : null}</div>
+				<input className='tag-input' type='text' placeholder='Add a tag' id={id} onKeyDown={this.handleChange} />
+			</>
+		);
+
+		return (
+			<div className='list-item'>
+				<img className='list-item__img' src={pic} alt={fullName} />
+				<div className='list-item__info'>
+					<h1 className='list-item__name'>
+						{firstName} {lastName}
+					</h1>
+					<ul className='list-item__list'>
+						<li className='list-item__li list-item__email'>Email: {email}</li>
+						<li className='list-item__li list-item__company'>
+							Company: {company}
+						</li>
+						<li className='list-item__li list-item__skill'>Skill: {skill}</li>
+						<li className='list-item__li list-item__average'>
+							Average: {average.toFixed(2)}%
+						</li>
+					</ul>
+					{this.state.isToggleGradesOn ? gradesAndTags : !gradesAndTags}
+				</div>
+				<div className='plus-minus-cont'>
+					{this.state.isToggleGradesOn ? (
+						<img
+							className='expand-btn'
+							src={minus}
+							alt='minimize'
+							onClick={this.handleClickGrades}
+						/>
+					) : (
+						<img
+							className='expand-btn'
+							src={plus}
+							alt='expand'
+							onClick={this.handleClickGrades}
+						/>
+					)}
+				</div>
+			</div>
+		);
+	}
+}
 
 export default ListItem;
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -4,45 +4,88 @@ import axios from 'axios';
 import ListItem from '../ListItem/ListItem';
 
 class Main extends React.Component {
-	state = { data: [], search: '' };
+	state = {
+    data: [], 
+    searchNames: '', 
+    searchTags: '',
+  };
 
 	getStudents = () => {
-    axios
-      .get(`https://www.hatchways.io/api/assessment/students`)
-      .then(res => {
-        this.setState({
-          data: res.data.students
-        });
-      });
+		axios.get(`https://www.hatchways.io/api/assessment/students`).then(res => {
+			this.setState({
+				data: res.data.students
+			});
+		});
+	};
+
+	searchNames = e => {
+		this.setState({ searchNames: e.target.value });
   };
   
-  search = (e) => {
-    this.setState({ search: e.target.value })
+  handleTagUpdate = (newTag, id) => {
+    this.setState(prevState => ({ data: this.state.data.map(student => {
+      if(student.id === id && prevState.tags) {
+        student.tags = prevState.tags.concat(newTag);
+      }
+      if(student.id === id && !prevState.tags) {
+        student.tags = [newTag];
+      }
+      return student;
+    }) }))
   }
+  //tags: prevState.tags.concat(newTag)
+
+	searchTags = e => {
+		this.setState({ searchTags: e.target.value });
+	};
 
 	componentDidMount() {
-		this.getStudents();
-  }
+    this.getStudents();
+    // this.setState({ data: this.state.data.map(student => {
+    //     return student.tags = [];
+    // }) })
+	}
 
 	render() {
+    console.log(this.state.data)
+		const filteredStudents = this.state.data.filter(student => {
+			const fullName = student.firstName + ' ' + student.lastName;
+			return fullName
+				.toLowerCase()
+				.includes(this.state.searchNames.toLowerCase());
+		});
 
-    const filteredStudents = this.state.data.filter(student => {
-      const fullName = student.firstName + ' ' + student.lastName;
-      return fullName.toLowerCase().includes(this.state.search.toLowerCase())
-    }) 
+		// const filteredByTags = this.state.data.filter(student => {
+		//   return student.tags.toLowerCase().includes(this.state.searchTags.toLowerCase())
+		// })
 
 		return (
-      <div className='container'>
-        <input className='name-input' id='name-input' type="text" placeholder='Search by name' onChange={this.search} />
-        <div className='student-list'>
-          {this.state.data ? (
-              filteredStudents.map(student => (
-                <ListItem key={student.id} student={student} />
-              ))
-            ) : <h4>No students found...</h4>}
-        </div>
-      </div>
-    )
+			<div className='container'>
+				<input
+					className='name-input'
+					id='name-input'
+					type='text'
+					placeholder='Search by name'
+					onChange={this.searchNames}
+				/>
+				<input
+					className='tag-search'
+					id='tag-search'
+					type='text'
+					placeholder='Search by tags'
+					onChange={this.searchTags}
+				/>
+				<div className='student-list'>
+					{this.state.data ? (
+						filteredStudents.map(student => (
+							<ListItem key={student.id} student={student} tags={this.state.tags} setTags={this.handleTagUpdate} />
+						))
+					) : (
+						<h4>No students found...</h4>
+					)}
+				</div>
+			</div>
+		);
 	}
 }
 
